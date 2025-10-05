c3c9aae9ccfeff91c7179572ad531f92c84a695b
BHUMIKA BAHUGUNA <158489676+BhumikaBahuguna@users.noreply.github.com>
2025-10-05 12:44:16 +0530

    Update rotateleftbyd2.cpp

--- rotateleftbyd2.cpp
+++ rotateleftbyd2.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-void reverse(int arr[],int start,int end){
+void reverse(vector<int>&arr,int start,int end){
     while(start<=end){
         int temp=arr[start];
         arr[start]=arr[end];
@@ -9,10 +9,10 @@ void reverse(int arr[],int start,int end){
         end--;
     }
 }
-void rotateleftbyd(vector<int>arr[],int n,int d){
-    reverse(arr,arr+d);
-    reverse(arr+d,arr+n);
-    reverse(arr,arr+n);
+void rotateleftbyd(vector<int>&arr,int n,int d){
+    reverse(arr,0,d-1);
+    reverse(arr,d,n-1);
+    reverse(arr,0,n-1);
 }
 int main(){
     int n,d;
@@ -24,7 +24,13 @@ int main(){
     cin>>d;
     d=d%n;
     rotateleftbyd(arr,n,d);
+    cout<<"array after rotation :";
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
     return 0;
 }
+
+
 
 
